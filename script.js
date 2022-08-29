4a146ae2b10a7c303fba21797f5ac54a0c53d4dd
Joseph Davis <jdavi195@gmail.com>
2022-08-29 04:33:28 -0400

    updates. 1.0.8
    Nav bar

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-alert("Hello World")
+//--alert("Hello World")--//
 
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
