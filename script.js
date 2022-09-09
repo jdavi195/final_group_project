a4e18a11980455d457a0ba4a3885ea9bdc560f2f
Joseph Davis <jdavi195@gmail.com>
2022-09-09 14:14:50 -0400

    updates

--- script.js
+++ script.js
@@ -7,17 +7,23 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
+var modalBtn = document.querySelector('.modalBtn');
 
-// Get the modal
+var modalbg = document.querySelector('.modal-bg');
+
+modalBtn = addEventListener('click',function(){
+  modalbg.classList.add('bg-active')
+
+})
+
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
+
 var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
 }
